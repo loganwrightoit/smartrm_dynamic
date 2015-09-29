599099974b9c591087638bae06e8c46a0745dabd
Logan Wright <logan.wright@oit.edu>
2015-09-28 19:49:29 -0500

    Add resource is now working.

--- src/srm/model/RegisteredResource.java
+++ src/srm/model/RegisteredResource.java
@@ -3,12 +3,13 @@ package srm.model;
 public class RegisteredResource
 {
 
-	public RegisteredResource(int rr_id, String rr_name, int l_id, int r_id, String special_features, int capacity)
+	public RegisteredResource(int rr_id, String rr_name, int l_id, int r_id, String r_name, String special_features, int capacity)
 	{
 		this.rr_id = rr_id;
 		this.rr_name = rr_name;
 		this.l_id = l_id;
 		this.r_id = r_id;
+		this.r_name = r_name;
 		this.special_features = special_features;
 		this.capacity = capacity;
 	}
@@ -62,11 +63,20 @@ public class RegisteredResource
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
+	
+	public String getR_name() {
+		return r_name;
+	}
+
+	public void setR_name(String r_name) {
+		this.r_name = r_name;
+	}
 
 	int rr_id;
 	String rr_name;
 	int l_id;
 	int r_id;
+	String r_name;
 	String special_features;
 	int capacity;
 	
